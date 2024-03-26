9c8d553c3162a3c8c129d2f7534b73099ac40f11
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-03-26 20:28:09 +0100

    Add protypes verification

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,11 @@
+import PropTypes from "prop-types";
+
+PokemonCard.propTypes ={
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,  //default there are all optionnal, see in protypes docs
+  }).isRequired,
+}
+
 
 function PokemonCard({pokemon}) {
   const noImg="???"
