e0dbae7271d8e8c52955ebb10dc553965b89af91
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-03-26 19:49:24 +0100

    use props to send pokemon data to pokemonCard from App

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,6 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCard() {
-  const pokemon=pokemonList[1];
+function PokemonCard({pokemon}) {
   const noImg="???"
-  console.log(pokemon);
     return <>
       {pokemon.imgSrc!==undefined? <img src={pokemon.imgSrc}/> : <p>{noImg}</p>}
       <figcaption>{pokemon.name}</figcaption>
