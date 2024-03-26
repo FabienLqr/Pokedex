42138a646486b8cda833300657fde7268d8b5184
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-03-26 16:37:13 +0100

    Create PokemonCard component on a new branch

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+function PokemonCard() {
+    return <>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+      <figcaption>Bulbasaur</figcaption>
+    </>;
+  }
+  
+  export default PokemonCard;
